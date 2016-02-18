a07a129c214390390e04ee55e53df955c00d9270
Kosjakov Evgeniy <42_@mail.ru>
2016-02-18 16:48:29 +0300

    Console app for report generation

---
+++ NunitGoReport/Program.cs
@@ -0,0 +1,13 @@
+﻿using NunitGo.Utils;
+
+namespace NunitGoReport
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = NunitGoHelper.Configuration;
+
+        }
+    }
+}
